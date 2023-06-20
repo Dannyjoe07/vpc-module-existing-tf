a796c7e5099c26b56e385a0a5b4272ee301aa0f6
Dannyjoe07 <dannyjoeanto@gmail.com>
2023-06-20 12:59:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,14 +3,13 @@ locals {
 #   db_subnets_ids    = flatten([data.aws_subnet_ids.dbA.*.ids, data.aws_subnet_ids.dbB.*.ids])
 #   appli_subnets_ids = flatten([data.aws_subnet_ids.appliA.*.ids, data.aws_subnet_ids.appliB.*.ids])
   public_subnets_ids = flatten([data.aws_subnets.publicA.*.ids, data.aws_subnets.publicB.*.ids])
-  tags_vpc = { Name = "${lower(var.vpc_name)}-vpc}" }
 
 }
 
 data "aws_vpc" "selected" {
   filter {
     name   = "tag:Name"
-    values = [local.tags_vpc.Name]
+    values = var.vpc_name
   }
 }
 
