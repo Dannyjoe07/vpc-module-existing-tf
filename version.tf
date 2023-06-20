b45b356c636afd30100c0c5d73f7ece8f8eb8b98
Dannyjoe07 <dannyjoeanto@gmail.com>
2023-06-20 12:36:52 +0530

    Update version.tf

--- version.tf
+++ version.tf
@@ -1,7 +1,7 @@
-terraform {
-  required_version = "~> 1.1.8"
+# terraform {
+#   required_version = "~> 1.1.8"
 
-  required_providers {
-    aws        = "~> 4.12.0"
- }
-}
+#   required_providers {
+#     aws        = "~> 4.12.0"
+#  }
+# }
