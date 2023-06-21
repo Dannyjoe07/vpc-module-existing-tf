f5f33925b653d563512f7b5813915280dca66690
Dannyjoe07 <dannyjoeanto@gmail.com>
2023-06-21 17:31:20 +0530

    backend

--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
 variable "vpc_name" {
   description = "Enter the vpc name to select the VPC"
 }
+
+variable "region" {
+  default = "us-east-1"
+}
--- version.tf
+++
@@ -1,7 +0,0 @@
-# terraform {
-#   required_version = "~> 1.1.8"
-
-#   required_providers {
-#     aws        = "~> 4.12.0"
-#  }
-# }
